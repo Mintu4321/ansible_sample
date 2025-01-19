9199e903426ac58c24108dd38139fca50b60621f
Tridib Bas <89687126+Mintu4321@users.noreply.github.com>
2025-01-19 23:10:51 +0530

    Create dockerLogin.groovy

---
+++ dockerLogin.groovy
@@ -0,0 +1,18 @@
+
+def dockerLogin(String credentialsId) {
+    try {
+       withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'username', passwordVariable: 'password')]) {
+        def result = sh(script: docker login -u ${username} -p ${password})
+        echo "docker login is successful"
+        return result
+       }
+        }
+    catch (Exception e) {
+        echo "Error occurred as: ${e.message}"
+
+
+
+    }
+
+}
+
