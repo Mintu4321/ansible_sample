bc5c691a08283297f881a7925e80cabe31997abe
Tridib Bas <89687126+Mintu4321@users.noreply.github.com>
2025-01-20 00:41:52 +0530

    Update dockerLogin.groovy

--- dockerLogin.groovy
+++ dockerLogin.groovy
@@ -3,10 +3,9 @@ def login(String credentialsId) {
         // Fetch credentials securely
         withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'username', passwordVariable: 'password')]) {
             // Execute docker login
-            result = sh(script: 'docker login -u "${username}" --password-stdin"', returnStdout: true).trim())
-            return result
-        
+           def result = sh(script: "docker login -u ${username} --password-stdin", returnStdout: true).trim()
             echo "Docker login is successful"
+            return result
             
         }
     } catch (Exception e) {
